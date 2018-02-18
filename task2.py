421030cba8a53703ffdeaee02b95bb2983dea42e
elijah1027 <31459838+elijah1027@users.noreply.github.com>
2018-02-18 00:58:25 -0500

    Update task2.py

--- task2.py
+++ task2.py
@@ -14,11 +14,17 @@ class MRWordCounter(MRJob):
                 MRStep(reducer=self.reducer_alpha)
             ]
     def mapper(self, key, line):
+        #Reference:
+        #https://docs.python.org/2/library/re.html(Link:findall)
+
+        #https://stackoverflow.com/questions/16312955/what-is-the-regular-expression-in-python-that-match-alphabet-only(Link: find alphabet)
         line_split = re.findall(r"[a-zA-Z]+",line)
         for word in line_split:
             yield word,1            
             
     def reducer_step1(self, word, counts):
+        #This step is referred to:
+        ##https://pythonhosted.org/mrjob/guides/writing-mrjobs.html(Link:MRjob tutorial)
         yield None, (sum(counts),word)
     
     def reducer_alpha(self, _, pairs):
